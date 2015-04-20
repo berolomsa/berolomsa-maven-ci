08d576443a1f4645a17440441eb2ce2af92f4dd6
BeroLomsa <bloms12@freeuni.edu.ge>
2015-04-21 01:11:00 +0400

    Mock Test Added

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -2,7 +2,11 @@
  * Created by MyBitch on 4/21/2015.
  */
 public class TestClass {
-	public static String inverse(String str){
+	public TestClass(){
+
+	}
+
+	public String inverse(String str){
 		String result = "";
 		for(int i=0; i<str.length();i++){
 			result+=str.charAt(str.length()-1-i);
--- src/test/java/TestClassTest.java
+++ src/test/java/TestClassTest.java
@@ -1,10 +1,20 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
 
 public class TestClassTest {
 	@Test
 	public void testInverse(){
-		String result = TestClass.inverse("Bero");
+		TestClass test = new TestClass();
+		String result = test.inverse("Bero");
 		assertEquals("oreB",result);
+	}
+	@Test
+	public void test1()  {
+		TestClass test = mock(TestClass.class);
+		// define return value for method getUniqueId()
+		when(test.inverse("Bero")).thenReturn("Lomsadze");
+		assertEquals(test.inverse("Bero"),"Lomsadze");
 	}
 }
